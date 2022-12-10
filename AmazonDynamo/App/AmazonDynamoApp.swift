4a51589132b0deebd7eb0e844f7dcc58802bd246
Mohamed Shimy <msh.work@outlook.sa>
2022-12-10 13:46:22 +0200

    Setup Project
     - Add .ignored file.
     - Organize project folders.

--- AmazonDynamo/App/AmazonDynamoApp.swift
+++ AmazonDynamo/App/AmazonDynamoApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct AmazonDynamoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                Text("AmazonDynamoApp")
+            }
         }
     }
 }
